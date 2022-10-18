ec99664641dcd2a17cef537a86454d3fc6daa405
Jone Doe <nurmaliki58@gmail.com>
2022-10-18 13:12:28 +0700

    penamabahan user add dan edit

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -40,4 +40,97 @@ class User extends CI_Controller
         $this->load->view('user/data', $data);
         $this->load->view('layout/footer');
     }
+
+
+    public function add()
+    {
+        if (!($this->session->userdata('data') != NULL)) {
+            redirect(base_url() . 'login');
+        }
+
+        $user = $this->db
+            ->select('
+               *
+            ')
+            ->from('dashboard_user')
+            ->join('role', 'role.id=dashboard_user.role_id')
+            ->get()
+            ->result();
+        $data = [
+            'user' => $user
+        ];
+        $this->load->view('layout/header');
+        $this->load->view('user/add', $data);
+        $this->load->view('layout/footer');
+    }
+
+    public function add_action()
+    {
+        if (!($this->session->userdata('data') != NULL)) {
+            redirect(base_url() . 'login');
+        }
+        $p = (object)$_POST;
+        $data = [
+            'username'      => $p->username,
+            'password'      => $p->password,
+            'role_id'       => $p->role_id,
+            'created_at'    => date('Y-m-d H:i:s'),
+        ];
+        $user = $this->db->insert('dashboard_user',$data);
+        if ($user) {
+            $this->session->set_flashdata('message_user', 'Sukses save');
+            redirect(base_url() . 'user');
+        } else {
+            $this->session->set_flashdata('message_user', 'Gagal save');
+            redirect(base_url() . 'user/add');
+        }
+        
+    }
+
+    public function edit($id)
+    {
+        if (!($this->session->userdata('data') != NULL)) {
+            redirect(base_url() . 'login');
+        }
+
+        $user = $this->db
+            ->select('
+               *
+            ')
+            ->from('dashboard_user')
+            ->join('role', 'role.id=dashboard_user.role_id')
+            ->where('id',$id)
+            ->get()
+            ->row();
+        $data = [
+            'user' => $user
+        ];
+        $this->load->view('layout/header');
+        $this->load->view('user/edit', $data);
+        $this->load->view('layout/footer');
+    }
+
+
+    public function edit_action($id)
+    {
+        if (!($this->session->userdata('data') != NULL)) {
+            redirect(base_url() . 'login');
+        }
+        $p = (object)$_POST;
+        $data = [
+            'username'      => $p->username,
+            'password'      => $p->password,
+            'role_id'       => $p->role_id,
+            // 'created_at'    => date('Y-m-d H:i:s'),
+        ];
+        $user = $this->db->update('dashboard_user',$data)->where('id',$id);
+        if ($user) {
+            $this->session->set_flashdata('message_user', 'Sukses save');
+            redirect(base_url() . 'user');
+        } else {
+            $this->session->set_flashdata('message_user', 'Gagal save');
+            redirect(base_url() . 'user/add');
+        }
+        
+    }
 }
---
+++ application/views/user/edit.php
@@ -0,0 +1,93 @@
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+      <?php $this->load->view('layout/content_header');  ?>
+
+      <!-- Main content -->
+      <section class="content">
+          <div class="container-fluid">
+              <div class="row">
+                  <div class="col-12">
+                      <div class="card">
+                          <div class="card-header">
+                              <!-- <h3 class="card-title">DataTable with minimal features & hover style</h3> -->
+                          </div>
+                          <!-- /.card-header -->
+                          <form action="<?= base_url() ?>/user/add_action" methode="POST">
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Username</label>
+                                        <input value="<?= $user->username?>" type="text" name="username" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword1">Password</label>
+                                        <input value="<?= $user->password?>" type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                                    </div>
+                                
+                                    <div class="form-group">
+                                        <label>Role</label>
+                                        <select name="role_id" class="custom-select">
+                                        <option value="2">Admin</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <!-- /.card-body -->
+
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </form>
+                          <!-- /.card-body -->
+                      </div>
+                      <!-- /.card -->
+
+                  </div>
+                  <!-- /.col -->
+              </div>
+              <!-- /.row -->
+          </div>
+          <!-- /.container-fluid -->
+      </section>
+      <!-- /.content -->
+  </div>
+
+
+<div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Quick Example</h3>
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              <form>
+                <div class="card-body">
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputFile">File input</label>
+                    <div class="input-group">
+                      <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="exampleInputFile">
+                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                      </div>
+                      <div class="input-group-append">
+                        <span class="input-group-text">Upload</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                  </div>
+                </div>
+                <!-- /.card-body -->
+
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+              </form>
+            </div>
